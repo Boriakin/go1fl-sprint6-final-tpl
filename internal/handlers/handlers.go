d444fa02cbba22f7eca382ff778c883a6b530812
Boriakin <boriakin97@gmail.com>
2026-01-14 02:10:17 +0200

    first commit

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,54 @@
 package handlers
+
+import (
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
+)
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "index.html")
+}
+
+func UploadHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseMultipartForm(10 << 20); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	dowFile, header, err := r.FormFile("myFile")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer dowFile.Close()
+
+	data, err := io.ReadAll(dowFile)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	convert, err := service.ConverterMorse(string(data))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	fileName := time.Now().UTC().String() + filepath.Ext(header.Filename)
+
+	outFile, err := os.Create(fileName)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	defer outFile.Close()
+
+	if _, err := outFile.WriteString(convert); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	http.ServeFile(w, r, fileName)
+}
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,31 @@
 package service
+
+import (
+	"errors"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+func ConverterMorse(message string) (string, error) {
+	if message == "" {
+		return "", errors.New("message is empty")
+	}
+
+	if isMorse(message) {
+		return morse.ToText(message), nil
+	}
+
+	return morse.ToMorse(message), nil
+}
+
+func isMorse(m string) bool {
+	for _, r := range m {
+		switch r {
+		case '.', '-', ' ':
+			continue
+		default:
+			return false
+		}
+	}
+	return len(m) > 0
+}
